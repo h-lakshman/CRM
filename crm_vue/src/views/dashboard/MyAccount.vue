70d3dbe4aa2c8bb78e17ad4d10631a0e754b7cb3
h-lakshman <lakshman.vaibhav007@gmail.com>
2023-09-10 23:13:13 +0530

    Created a signup,login,dashboard and myaccount pages and integrated it with django rest api's and configured the project.

---
+++ crm_vue/src/views/dashboard/MyAccount.vue
@@ -0,0 +1,43 @@
+<template>
+    <div class="container">
+        <div class="columns is-multiline">
+            <div class="column is-12">
+                <h1 class="title">My Account</h1>
+            </div>
+
+            <div class="column is-12">
+                <div class="buttons">
+                    <!--
+                    <router-link :to="{ name: 'EditMember', params: { id: $store.state.user.id }}" class="button is-light">Edit</router-link>
+                    -->
+                    <button @click="logout()" class="button is-danger">Log out</button>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    import axios from 'axios'
+    export default {
+        name: 'MyAccount',
+        methods: {
+            async logout(){
+                await axios
+                    .post('/api/v1/token/logout/')
+                    .then(response => {
+                        console.log('Logged out')
+                    })
+                    .catch(error => {
+                        console.log(JSON.stringify(error))
+                    })
+                axios.defaults.headers.common['Authorization'] = ''
+                localStorage.removeItem('token')
+                localStorage.removeItem('removeToken')
+
+                this.$router.push('/')
+                
+        }
+    }
+}
+</script>
